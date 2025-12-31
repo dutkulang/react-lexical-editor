1612da22bc2d60e5139aef9588fe537d4089483b
Dut Kulang <dutandrew78@gmail.com>
2025-12-31 19:15:51 +0200

    Registered Editor to App interface

--- react-lexical/src/App.jsx
+++ react-lexical/src/App.jsx
@@ -3,7 +3,7 @@ import Editor from "./components/Editor";
 function App() {
   return (
     <>
-	  <h1>Rich Text Editor</h1>
+	  <h1 className="my-4 text-2xl text-center font-bold">Rich Text Editor</h1>
 	  <Editor/>
     </>
   )
